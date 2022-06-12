ae2f215393b0b757db49141d9f4ba34b2f4b8d53
Ania <ann.lejman@gmail.com>
2022-06-12 11:37:50 +0200

    pierwszy commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './App.module.css';
+
+function App() {
+    return (
+        <div className={classes.NaglowekStrony}>
+            <div className={classes.LewaStrona}>
+                <div className={classes.Logos}>
+                    <div> Logo</div>
+                    <div> Logo</div>
+                    <div> Logo</div>
+                </div>
+                <div className={classes.Tytul}>Tytuł strony</div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
